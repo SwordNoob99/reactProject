0743d1ea625304c394b761e1b6906ac5c70f5805
oussama <oussama.elyazami@3pos.de>
2022-05-11 17:27:24 +0100

    project component

--- src/app/form-elements/Project.js
+++ src/app/form-elements/Project.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
 import { Form } from 'react-bootstrap';
 import DatePicker from "react-datepicker";
-import bsCustomFileInput from 'bs-custom-file-input'
+import bsCustomFileInput from 'bs-custom-file-input';
+import axios from 'axios';
 
 export class Project extends Component {
   state = {
-    startDate: new Date()
-  };
- 
-  handleChange = date => {
-    this.setState({
-      startDate: date
-    });
-  };
+    name: '' ,
+
+  }
+
+  handleChange = event => {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const user = {
+      name: this.state.name
+    };
+
+    axios.post(`https://jsonplaceholder.typicode.com/users`, { user })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      });
+    }
   componentDidMount() {
     bsCustomFileInput.init()
-    }
+      var data
+
+      axios.get(`http://127.0.0.1:8082/api/v1/Project/projects`)
+        .then(res => { console.log(res)
+          
+         
+        })
+        
+    
+    };
   render() {
     return (
       <div>
@@ -40,7 +63,7 @@ export class Project extends Component {
                 <form className="forms-sample">
                   <Form.Group>
                     <label htmlFor="exampleInputName1">Name</label>
-                    <Form.Control type="text" className="form-control" id="exampleInputName1" placeholder="Name" />
+                    <Form.Control type="text" className="form-control" id="exampleInputName1" placeholder="Name" name='name' />
                   </Form.Group>
                   <Form.Group>
                     <label htmlFor="exampleSelectGender">Lot De travaus</label>
@@ -60,7 +83,7 @@ export class Project extends Component {
                   <Form.Group>
                     <label>File upload</label>
                     <div className="custom-file">
-                      <Form.Control type="file" className="form-control visibility-hidden" id="customFileLang" lang="es"/>
+                      <Form.Control type="file" className="form-control visibility-hidden" id="customFileLang" lang="es" name='img_path'/>
                       <label className="custom-file-label" htmlFor="customFileLang">Upload image</label>
                     </div>
                   </Form.Group>
@@ -70,9 +93,9 @@ export class Project extends Component {
                   </Form.Group>
                   <Form.Group>
                     <label htmlFor="exampleTextarea1">Textarea</label>
-                    <textarea className="form-control" id="exampleTextarea1" rows="4"></textarea>
+                    <textarea className="form-control" id="exampleTextarea1" name='description' rows="4"></textarea>
                   </Form.Group>
-                  <button type="submit" className="btn btn-primary mr-2">Submit</button>
+                  <button type="submit" className="btn btn-primary mr-2" >Submit</button>
                   <button className="btn btn-light">Cancel</button>
                 </form>
               </div>
